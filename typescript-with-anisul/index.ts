bfcdf158cb153a5ba28e902f23253806c5295f28
MdTasib <ohidulalam442@gmail.com>
2022-07-24 18:53:25 +0600

    first program

---
+++ typescript-with-anisul/index.ts
@@ -0,0 +1,38 @@
+/**
+ *
+ * Class - ( 02 ) 
+ * First Program
+ *
+ * 
+ */
+
+/* let name:string = 'Ohidul alam';
+// name = 30; // error
+let age:number = 20;
+// age = "20"; // error
+console.log(name);
+console.log(age); */
+
+/**
+ * Function
+ */
+/* function addNumber(num1, num2){
+ console.log(num1 + num2);
+}
+addNumber(20, "30"); */
+
+/* 
+function addNumber(num1:number, num2:number){
+ console.log(num1 + num2);
+}
+addNumber(20, 30);
+addNumber(20, "30"); // run time error */
+
+
+/**
+ *
+ * Class - ( 02 ) 
+ * First Program
+ *
+ * 
+ */
