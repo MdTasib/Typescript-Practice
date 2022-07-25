c50651c20698b4db5dda440f680962481aaa1355
MdTasib <ohidulalam442@gmail.com>
2022-07-25 18:32:04 +0600

    tuple data type

---
+++ typescript-with-anisul/user-defined-type/tuple-type.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * class ( 07 )
+ * Tuple Data Type
+ * --- Mixed data type
+ *
+ */
+var user;
+user = [101, 'Tasib'];
+// user = ['Rafi', 102]; // Error
+console.log(user);
---
+++ typescript-with-anisul/user-defined-type/tuple-type.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * class ( 07 )
+ * Tuple Data Type
+ * --- Mixed data type
+ * 
+ */
+
+let user: [number, string];
+user  = [101, 'Tasib'];
+// user = ['Rafi', 102]; // Error
+
+console.log(user);
